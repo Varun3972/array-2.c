ced0acb9c6a78cebc6a7905b33e8540ce9b30d66
Varun3972 <96814526+Varun3972@users.noreply.github.com>
2022-01-18 11:05:31 +0530

    Create Array 2.c

---
+++ Array 2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int a[100],i,n;
+    printf("Enter the size of the array");
+    scanf("%d",&n);
+    printf("Enter the elements of an array");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("The elements are\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t",a[i]);
+    }
+    
+}
